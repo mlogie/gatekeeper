e608e90322287718c5960c8f0e7ae31cf6b69f38
mlogie <marlog@ceh.ac.uk>
2017-11-15 17:15:44 +0000

    Working read xls code, saving off to dummy data frame

--- ReadXls.R
+++ ReadXls.R
@@ -5,20 +5,20 @@
 ##########################################################################################
 ## First, load up the gdata library, for reading xlsx files
 install.packages('gdata')
-install.packages('xlsx')
+#install.packages('xlsx')
 library('gdata')
-library('xlsx')
+#library('xlsx')
 
 ## Set the file path
 datapath <- file.path('.','Farm_data')
+perl <- 'C:/Strawberry/perl/bin/perl5.26.1.exe'
 ## Discover all the folders in the data folder
 FolderList <- list.files(datapath)
-perl <- 'C:/Users/marlog/Downloads'
+
 ## Create a list of all the files in folder 1, and save just those in xls format
 FirstList <- list.files(file.path(datapath,FolderList[1]))
 FirstList <- FirstList[grepl('.xls$',FirstList)]
-file.path(datapath,FolderList[1],FirstList[1])
-mydf <- read.xls(file.path(datapath,FolderList[1],FirstList[1]),sheet = 'Sheet1')
-read.xlsx("./Farm_data/Druids Lodge/Detailed Operations camp and flinty pond 2013.xls")
 
-list.files(path)
+mydf <- read.xls(file.path(datapath,FolderList[1],FirstList[1]),sheet = 'Sheet1',
+                 perl = perl)
+
